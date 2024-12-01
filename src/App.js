f06982c1d8b96e1ba463c872f05dc99452511853
ShivaSredu <saiprasad.srgroup@gmail.com>
2024-12-01 18:40:16 +0530

    Initial Commit Project Setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>React Application Deployement</h1>
+        <p>
+          This Application I Have Deployemnt Into Kubernetes Using Github
+          Webhooks
+        </p>
+        <p>Ec2 Jenkins Server, Docker, Dokcer Registry & Kubernetes</p>
+      </div>
+      <div className="App">
+        <h1>Git, Jenkins, Docker & Kubernetes</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
